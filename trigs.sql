b883cb72330430f7b0f9c962bbe08f85469734d2
Troynikova Veronika <troynikv@gmail.ru>
2023-11-20 15:20:22 +0300

    Add start discussion func

--- trigs.sql
+++ trigs.sql
@@ -129,3 +129,66 @@ $$ LANGUAGE plpgsql;
 	
 CREATE TRIGGER check_data_for_accusation BEFORE INSERT OR UPDATE ON accusation
     FOR EACH ROW EXECUTE FUNCTION check_data_for_accusation();
+
+
+CREATE OR REPLACE FUNCTION get_best_principal(cur_locality_id integer, cur_official_name official_name) RETURNS integer AS $$
+	DECLARE
+		principal					 integer;
+	BEGIN
+		 SELECT id INTO principal FROM official
+			JOIN person on person_id = person.id
+			WHERE official_name = cur_official_name and locality_id = cur_locality_id and NOT EXISTS (
+				SELECT 1 FROM case_log WHERE official.id = principal)
+			LIMIT 1;
+
+		IF principal IS NOT NULL THEN
+			RETURN principal;
+		END IF;
+
+		SELECT official.id INTO principal
+		FROM official
+		JOIN case_log ON official.id = case_log.principal
+		JOIN person on person_id = person.id
+		WHERE finish_time IS NOT NULL and official_name = cur_official_name and locality_id = cur_locality_id
+		GROUP BY official.id
+		HAVING COUNT(*) = (SELECT COUNT(*) FROM case_log WHERE principal = official.id)
+		LIMIT 1;
+
+		IF principal IS NOT NULL THEN
+			RETURN principal;
+		END IF;
+
+		SELECT case_log.principal INTO principal
+		FROM case_log
+		JOIN official on official.id = case_log.principal
+		JOIN person on person_id = person.id
+		WHERE official_name = cur_official_name and locality_id = cur_locality_id
+		GROUP BY case_log.principal
+		ORDER BY COUNT(*) ASC
+		LIMIT 1;
+
+		RETURN principal;
+END;
+$$ LANGUAGE plpgsql;
+		
+
+CREATE OR REPLACE PROCEDURE start_discution(cur_case_id integer, discription text) RETURNS integer AS $$
+	IS
+		principal					 integer;
+		locality_id					 integer;
+		new_case_log_id				 integer;
+    BEGIN
+		locality_id = ( select locality_id from church 
+							join inquisition_process on church_id = church.id
+							join accusation inquisition_process_id = inquisition_process.id
+							join accusation_record id_accusation = accusation.id 
+							where accusation_record_id in (
+								select record_id from accusation_investigative_case where case_id = cur_case_id) limit 1);
+		principal = get_best_principal(locality_id, 'Епископ'); 
+		
+		INSERT INTO case_log (case_id, case_status, principal, start_time, result, prison_id, finish_time, 
+		punishment_id, description) VALUES (cur_case_id, 'Исправительная беседа', principal, CURRENT_TIMESTAMP, NULL, NULL, NULL, NULL, description)
+		RETURNING id INTO new_case_log_id;
+        DBMS_OUTPUT.PUT_LINE(new_case_log_id);
+    END;
+$$ LANGUAGE plpgsql;
